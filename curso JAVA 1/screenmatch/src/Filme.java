a94c77f8f00a417b51153f121243660535034549
dudaordahy <dudasciortino@gmail.com>
2025-06-10 23:32:49 -0300

    atualização JAVA

---
+++ curso JAVA 1/screenmatch/src/Filme.java
@@ -0,0 +1,10 @@
+public class Filme {
+    public static void main(String[] args) {
+        String nome;
+        int anoLacamento;
+        boolean incluidoNoPlano;
+        double avaliacao;
+        int totalAvaliacao;
+        int duracaoEmMinutos;
+    }
+}
